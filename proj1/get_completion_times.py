59a4f87fd13854599dfff5d6108f478825b7cde7
Jeff Rasley <jeffra45@gmail.com>
2014-02-18 21:33:51 -0500

    minor change

--- proj1/get_completion_times.py
+++ proj1/get_completion_times.py
@@ -1,6 +1,6 @@
 import time
 
-fd = open('Batch_1411601_batch_results.csv')
+fd = open('prof_meta_data_raw.csv')
 
 header = fd.readline()
 header = header.split('","')
@@ -17,5 +17,6 @@ for d in lines:
     tses.append(ts)
 
 m = min(tses)
+tses.sort()
 for d in tses:
     print (d-m) / 60 / 60 #hours
